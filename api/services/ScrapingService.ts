ba2edf15d3dbf40c80b06870ca49b1de4113e906
diegossl <diegossl94@gmail.com>
2020-07-11 00:41:07 -0300

    bug

--- api/services/ScrapingService.ts
+++ api/services/ScrapingService.ts
@@ -3,11 +3,19 @@ import Cheerio from 'cheerio'
 
 const url = 'https://www.tudogostoso.com.br/categorias'
 
+interface DataObject {}
+
 export default {
-  async getCategories (): Promise<unknown> {
+  async getCategories (): Promise<DataObject> {
     const pageData = await Axios.get(url)
     const $ = Cheerio.load(pageData.data)
-    const title = $('.box-title').text()
-    return { title: title }
+    const data: DataObject  = []
+
+    $('.page-tags > .row').map((i, dataBlock) => {
+      const link = $(dataBlock).find('h3 > a').attr('href')
+      data.push({ link: link })
+    }).get()
+    
+    return data
   }
 }
